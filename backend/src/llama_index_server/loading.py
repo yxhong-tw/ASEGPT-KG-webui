10bff27f68ec9c95d964a47bad3004eee5ecb7d2
Mark Hsu <zhiaohsu@gmail.com>
2024-04-08 18:31:35 +0800

    feat: Update local model usage

--- backend/src/llama_index_server/loading.py
+++ backend/src/llama_index_server/loading.py
@@ -1,8 +1,7 @@
 import json
 import os
-from typing import List, Literal, Tuple
+from typing import List, Literal, Tuple, Union
 
-from app.utils.data_model import KnwoledgeGraphQueryRequest
 from dotenv import load_dotenv
 from llama_index.core import (
     Document,
@@ -16,7 +15,6 @@ from llama_index.core import (
 )
 from llama_index.core.graph_stores.types import GraphStore
 from llama_index.core.indices.base import BaseIndex
-from llama_index.core.objects import ObjectIndex, SimpleToolNodeMapping
 from llama_index.core.query_engine import BaseQueryEngine, RetrieverQueryEngine
 from llama_index.core.retrievers import KGTableRetriever, VectorIndexRetriever
 from llama_index.core.tools import QueryEngineTool
@@ -25,48 +23,56 @@ from llama_index.llms.openai import OpenAI
 from nebula3.Config import Config
 from nebula3.gclient.net import ConnectionPool
 
-from .my_retriever import CustomLLM, CustomRetriever
+from .constants import (
+    NEBULA_EDGE_TYPES,
+    NEBULA_REL_PROP_NAMES,
+    NEBULA_STORE_TAGS,
+    SYSTEM_PROMPT,
+)
+from .model import CustomLLM, CustomRetriever
 
 load_dotenv()
 
-STORE_EDGE_TYPES, STORE_REL_PROP_NAMES = ['relationship'], ['relationship']
-STORE_TAGS = ['entity']
 
-
-def load_store(space_name: str) -> Tuple[NebulaGraphStore, bool]:
+def connect_to_nebula_graph() -> bool:
     # connect to nebula graph
     config = Config()
     config.max_connection_pool_size = 10
     connection_pool = ConnectionPool()
     ok = connection_pool.init([('graphd', 9669)], config)
 
+    return ok
+
+
+def load_store(space_name: str) -> NebulaGraphStore:
     graph_store = NebulaGraphStore(
         space_name=space_name,
-        edge_types=STORE_EDGE_TYPES,
-        rel_prop_names=STORE_REL_PROP_NAMES,
-        tags=STORE_TAGS,
+        edge_types=NEBULA_EDGE_TYPES,
+        rel_prop_names=NEBULA_REL_PROP_NAMES,
+        tags=NEBULA_STORE_TAGS,
     )
 
-    return graph_store, ok
+    return graph_store
 
 
-def load_service_and_storage(
-        store: GraphStore,
-        persist_dir: str) -> Tuple[ServiceContext, StorageContext]:
-    llm = OpenAI(temperature=0.1, model='gpt-3.5-turbo')
-    # llm = LLMPredictor(llm=CustomLLM())
-    service_context = ServiceContext.from_defaults(llm=llm, chunk_size=4096)
+def set_global_service(chunk_size: int = 4096,
+                       using_openai_gpt: bool = False,
+                       local_model_path: str = None,
+                       local_tokenizer_path: str = None) -> ServiceContext:
 
-    storage_context = StorageContext.from_defaults(persist_dir=persist_dir,
-                                                   graph_store=store)
+    if using_openai_gpt:
+        llm = OpenAI(temperature=0.1, model='gpt-3.5-turbo')
+    else:
+        llm = CustomLLM(model_path=local_model_path,
+                        tokenizer_path=local_tokenizer_path,
+                        max_new_tokens=2048)
 
-    return service_context, storage_context
-
-
-def set_global_service() -> ServiceContext:
-    llm = OpenAI(temperature=0.1, model='gpt-3.5-turbo')
-    # llm = LLMPredictor(llm=CustomLLM())
-    service_context = ServiceContext.from_defaults(llm=llm, chunk_size=4096)
+    # embed_model = LangchainEmbedding(HuggingFaceEmbeddings(model_name='sentence-transformers/all-mpnet-base-v2'))
+    service_context = ServiceContext.from_defaults(
+        llm=llm,
+        # embed_model=embed_model
+        chunk_size=chunk_size,
+        system_prompt=SYSTEM_PROMPT)
 
     Settings.llm = llm
 
@@ -87,9 +93,9 @@ def load_index(documents: List[Document], storage_context: StorageContext,
                                         service_context=service_context,
                                         max_triplets_per_chunk=100,
                                         space_name=space_name,
-                                        edge_types=STORE_EDGE_TYPES,
-                                        rel_prop_names=STORE_REL_PROP_NAMES,
-                                        tags=STORE_TAGS,
+                                        edge_types=NEBULA_EDGE_TYPES,
+                                        rel_prop_names=NEBULA_REL_PROP_NAMES,
+                                        tags=NEBULA_STORE_TAGS,
                                         verbose=True,
                                         include_embeddings=True,
                                         show_progress=True)
@@ -111,9 +117,9 @@ def load_multi_doc_index(documents: List[List[Document]],
                 storage_context=context,
                 max_triplets_per_chunk=100,
                 space_name=space_names[i],
-                edge_types=STORE_EDGE_TYPES,
-                rel_prop_names=STORE_REL_PROP_NAMES,
-                tags=STORE_TAGS,
+                edge_types=NEBULA_EDGE_TYPES,
+                rel_prop_names=NEBULA_REL_PROP_NAMES,
+                tags=NEBULA_STORE_TAGS,
                 verbose=True,
                 include_embeddings=True,
                 show_progress=True)
@@ -169,46 +175,6 @@ def load_engine(kg_index: KnowledgeGraphIndex,
                                     response_synthesizer=response_synthesizer)
 
 
-def load_documents(start_index: int = 0,
-                   end_index: int = None) -> List[Document]:
-    documents = []
-    with open(f'{os.getcwd()}/app/data/中美貿易戰禁令-all.json') as f:
-        json_data = json.load(f)[:500]
-
-        for doc in json_data:
-            # if doc['source_name'] in ['Ptt', '高雄市Open1999']:
-            #     continue
-            # if '半導體' not in doc['article_content']:
-            #     continue
-
-            document = Document(text=doc['article_content'],
-                                metadata={
-                                    'crawl_datetime':
-                                    doc['crawl_datetime'],
-                                    'source_name':
-                                    doc['source_name'],
-                                    'source_category':
-                                    doc['source_category'],
-                                    'article_url':
-                                    doc['article_url'],
-                                    'article_title':
-                                    doc['article_title'],
-                                    'article_author':
-                                    doc['article_author'],
-                                    'article_creation_date':
-                                    doc['article_creation_date'],
-                                },
-                                excluded_llm_metadata_keys=[
-                                    'crawl_datetime', 'source_name',
-                                    'source_category', 'article_url',
-                                    'article_title', 'article_author',
-                                    'article_creation_date'
-                                ])
-            documents.append(document)
-
-    return documents
-
-
 def load_multi_docs(file_names: List[str]) -> List[List[Document]]:
     documents = []
     for file_name in file_names:
@@ -216,7 +182,10 @@ def load_multi_docs(file_names: List[str]) -> List[List[Document]]:
         with open(f'{os.getcwd()}/app/data/{file_name}') as f:
             json_data = json.load(f)
             if 'semiconductor' not in file_name:
-                json_data = json_data[:500]
+                if len(json_data) >= 500:
+                    json_data = json_data[:500]
+                else:
+                    json_data = json_data[:-1]
 
             for doc in json_data:
                 if 'semiconductor' in file_name:
@@ -256,9 +225,16 @@ def load_multi_docs(file_names: List[str]) -> List[List[Document]]:
     return documents
 
 
-def load_object_index(all_tools: List[QueryEngineTool]):
-    tool_mapping = SimpleToolNodeMapping.from_objects(all_tools)
-    obj_index = ObjectIndex.from_objects(all_tools, tool_mapping,
-                                         KnowledgeGraphIndex)
+def convert_to_query_engine_tool(
+    query_engines: Union[BaseQueryEngine, List[BaseQueryEngine]],
+    names: Union[str, List[str]], descriptions: Union[str, List[str]]
+) -> Union[QueryEngineTool, List[QueryEngineTool]]:
 
-    return obj_index
+    assert type(query_engines) == type(names) == type(descriptions)
+
+    assert len(query_engines) == len(names) == len(descriptions)
+
+    return [
+        QueryEngineTool.from_defaults(query_engine=e, name=n, description=d)
+        for e, n, d in zip(query_engines, names, descriptions)
+    ]
