f9de9c2196f46794171c27c9a45efb54620628a1
zhiao777774 <zhiaohsu@gmail.com>
2024-08-20 01:43:24 +0800

    feat: Add 'set_rag_default_config' function

--- backend/src/utils/data_util.py
+++ backend/src/utils/data_util.py
@@ -6,3 +6,26 @@ def load_config(file_path: str):
         config_file = yaml.load(f, Loader=yaml.FullLoader)
 
     return config_file
+
+
+def set_rag_default_config(config):
+    if 'data' not in config:
+        config['data'] = {}
+    if 'nebula_graph' not in config:
+        config['nebula_graph'] = {}
+    if 'rag' not in config:
+        config['rag'] = {}
+
+    if 'document_paths' not in config['data']:
+        config['data']['document_paths'] = []
+    if 'space_names' not in config['nebula_graph']:
+        config['nebula_graph']['space_names'] = []
+    if 'persist_dirs' not in config['nebula_graph']:
+        config['nebula_graph']['persist_dirs'] = []
+
+    if 'generator_model_path' not in config['rag']:
+        config['rag']['generator_model_path'] = ''
+    if 'generator_tokenizer_path' not in config['rag']:
+        config['rag']['generator_tokenizer_path'] = ''
+
+    return config
